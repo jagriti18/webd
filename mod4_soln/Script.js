59f671dd1c6e5398ce589b1248ac466ac6c40835
jagriti18 <64573014+jagriti18@users.noreply.github.com>
2020-06-23 15:55:09 +0530

    Update Script.js

--- mod4_soln/Script.js
+++ mod4_soln/Script.js
@@ -1,4 +1,4 @@
-(function(window){
+(function(){
 var names = ["jason","neha","ashi","kriti","jack","jill","jaspreet"];
 for(var i=0;i<names.length;i++){
     var firstletter = names[i].charAt(0).toLowerCase();
@@ -9,4 +9,4 @@ for(var i=0;i<names.length;i++){
     }
 
 }
-})(window);
+})();
